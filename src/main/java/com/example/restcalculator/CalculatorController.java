86c689549c522c1b10eac82a4b315c9b210de34e
Leona Widhalm <wi21b101@technikum-wien.at>
2022-05-19 12:24:46 +0200

    add sum function

---
+++ src/main/java/com/example/restcalculator/CalculatorController.java
@@ -0,0 +1,16 @@
+package com.example.restcalculator;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @RequestMapping("/sum")
+    public int sum (
+            @RequestParam(name = "x") int a,
+            @RequestParam int b){
+        return a + b;
+    }
+}
